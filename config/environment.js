222e8d3656fc4ab3bde7ff7a1c5c444de4fc0525
nimmaj <git@nimmaj.com>
2014-12-28 13:29:05 +0000

    openshift changes
    these changes are pure hacks to see whether i can get gatewayd running
    on open shift.  if they work we can look fora less intrusive way of
    doing this

--- config/environment.js
+++ config/environment.js
@@ -18,8 +18,8 @@ nconf.defaults({
   'USER_AUTH': false, // Enable user routes with name/password basic auth
   'WEBAPP': true,
   'WEBAPP_PATH': __dirname + '/../node_modules/gatewayd-admin/',
-  'PORT': 5000, // Port of http api server
-  'HOST': 'localhost',
+  'PORT': process.env.OPENSHIFT_NODEJS_PORT || 5000, // Port of http api server
+  'HOST': process.env.OPENSHIFT_NODEJS_IP || 'localhost',
   'DOMAIN': 'example.com', // Domain of gateway, for ripple.txt and auth
   'HOT_WALLET': { address: false, secret: false }, // Required
   'COLD_WALLET': null, // Required
@@ -36,8 +36,8 @@ var dbConfig = DBConfigFile[nconf.get('NODE_ENV')];
 nconf.set('DATABASE_USER', dbConfig.user);
 nconf.set('DATABASE_PASSWORD', dbConfig.password);
 nconf.set('DATABASE_NAME', dbConfig.database);
-nconf.set('DATABASE_HOST', dbConfig.host);
-nconf.set('DATABASE_PORT', dbConfig.port);
+nconf.set('DATABASE_HOST', process.env.OPENSHIFT_POSTGRESQL_DB_HOST || dbConfig.host);
+nconf.set('DATABASE_PORT', process.env.OPENSHIFT_POSTGRESQL_DB_PORT || dbConfig.port);
 nconf.set('DATABASE_DIALECT', dbConfig.dialect);
 nconf.set('DATABASE_LOGGING', dbConfig.logging);
 nconf.set('DATABASE_URL', null);
